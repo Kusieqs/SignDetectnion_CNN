b0142c38148ff9254761c0c6865f622ee6861ab5
kusieqs <Konrad.kus@edu.uekat.pl>
2025-03-10 22:42:26 +0100

    Add new classes

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import os
+
 from utils import data_split
 from utils.preparation_data import apply_and_save_augmentations
 from model import  compile_model
 
 if __name__ == "__main__":
-    input_folder = r"C:\Users\konra\PycharmProjects\CNN_SD\Znaki\classification"
-    output_folder = r"C:\Users\konra\PycharmProjects\CNN_SD\MainSigns"
+    input_folder = "Znaki\classification"
+    output_folder = "MainSigns"
 
-    apply_and_save_augmentations(input_folder, output_folder)
+    if len(os.listdir(output_folder)) == 0:
+        print("Augmentation data...")
+        apply_and_save_augmentations(input_folder, output_folder)
 
-    source_dir = r'C:\Users\konra\PycharmProjects\CNN_SD\MainSigns'
-    data_dir = r'C:\Users\konra\PycharmProjects\CNN_SD\DataSplit'
+    source_dir = 'MainSigns'
+    data_dir = 'DataSplit'
 
-    data_split.split_data(source_dir, data_dir)
+    if len(os.listdir(source_dir)) == 0:
+        print("Spliting data...")
+        data_split.split_data(source_dir, data_dir)
+
     compile_model(data_dir)
 
 
--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+import time
+
 from ultralytics import YOLO
 import cv2
 import tensorflow as tf
@@ -5,40 +7,53 @@ import numpy as np
 from utils.constants import CLASS_NAMES, SIZE, MODELS_DICT
 
 yolo_model = YOLO("runs/detect/train6/weights/best.pt")
-classifier_model = tf.keras.models.load_model("0.955.keras")
+classifier_model = tf.keras.models.load_model("0.942.keras")
 
 
-image_path = r"test.png"
-image = cv2.imread(image_path)
+video_path = r"1.mp4"
+cap = cv2.VideoCapture(video_path)
 
-results = yolo_model(image)
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-for result in results:
-    for box in result.boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
-        cropped_sign = image[y1:y2, x1:x2]
+frame_count = 0
+skip_frames = 2
 
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-        sign = cv2.resize(cropped_sign, SIZE)
-        cv2.imshow("sign", sign)
-        cv2.waitKey(0)
+    frame_count += 1
+    if frame_count % skip_frames != 0:
+        continue
 
-        method = MODELS_DICT.get("EfficientNetB0")[1]
-        img_preprocessed = method(sign)
-        img_array = np.expand_dims(img_preprocessed, axis=0)
+    frame = cv2.resize(frame, (640, 416))
+    results = yolo_model(frame)
 
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            cropped_sign = frame[y1:y2, x1:x2]
 
-        prediction = classifier_model.predict(img_array)
-        class_id = np.argmax(prediction)
-        label = CLASS_NAMES.get(class_id, "Unknown")
+            if cropped_sign.size == 0:
+                continue
 
+            sign = cv2.resize(cropped_sign, SIZE)
+            method = MODELS_DICT.get("EfficientNetB0")[1]
+            img_preprocessed = method(sign)
+            img_array = np.expand_dims(img_preprocessed, axis=0)
 
+            prediction = classifier_model.predict(img_array)
+            class_id = np.argmax(prediction)
+            label = CLASS_NAMES.get(class_id, "Unknown")
 
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(image, f"{label} ({prediction[0][class_id]:.2f})", (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.putText(frame, f"{label} ({prediction[0][class_id]:.2f})",
+                        (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-# 7️⃣ Zapisz lub pokaż obraz z wynikami
-cv2.imshow("Detection & Classification", image)
-cv2.waitKey(0)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
