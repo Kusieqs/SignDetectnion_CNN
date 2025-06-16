158bb64d8ecc03f26da71e407ef9ce93c82462b5
kusieqs <Konrad.kus@edu.uekat.pl>
2025-06-16 20:41:10 +0200

    Change constants of YOLO method

--- YoloPredict/model.py
+++ YoloPredict/model.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 from constants import *
 model = YOLO("yolov8n.pt")
-model.train(data=r"C:\Users\konra\PycharmProjects\CNN_SD\YoloPredict\data.yaml", epochs=EPOCHS, imgsz=SIZE, batch=BATCH_SIZE)
+model.train(data=YAML_PATH, epochs=EPOCHS, imgsz=SIZE, batch=BATCH_SIZE)
